20802eaa75fc3f82a6be2dd2ebd87e09ae5e1cb7
Alexandro Yule Valencia <alexandroyule@gmail.com>
2024-03-26 02:06:37 -0500

    Commune Edit v2.

--- resources/views/comuna/edit.blade.php
+++ resources/views/comuna/edit.blade.php
@@ -11,21 +11,24 @@
     @csrf
     <div class="mb-3">
         <label for="id" class="form-label">Code</label>
-        <input type="text" class="form-control" id="id" aria-describedby="idHelp" name="id" disabled>
+        <input type="text" class="form-control" id="id" aria-describedby="idHelp" name="id" disabled value="{{$comuna->comu_codi}}">
         <div id="idHelp" class="form-text">Comune Code</div>
     </div>
     <div class="mb-3">
         <label for="name" class="form-label">Commune</label>
-        <input type="text" class="form-control" id="name" aria-describedby="nameHelp" name="comu_nomb" placeholder="Commune Name">
+        <input type="text" class="form-control" id="name" aria-describedby="nameHelp" name="comu_nomb" placeholder="Commune Name" value="{{$comuna->comu_nomb}}">
     </div>
     <div class="mb-3">
         <label for="municipality" class="form-label">Municipality</label>
         <select class="form-select" name="muni_codi" id="municipality" required>
-            <option selected disabled value="">Choose one...</option>
-            @foreach ($municipios as $municipio )
-                <option value="{{$municipio->muni_codi}}">{{$municipio->muni_nomb}}</option>
+            <option disabled value="">Choose one...</option>
+            @foreach ($municipios as $municipio)
+                <option value="{{ $municipio->muni_codi }}" {{ $municipio->muni_codi == $comuna->muni_codi ? 'selected' : '' }}>
+                    {{ $municipio->muni_nomb }}
+                </option>
             @endforeach
         </select>
+        
     </div>
     <div class="mt-3">
         <button type="submit" class="btn btn-primary">Update</button>
