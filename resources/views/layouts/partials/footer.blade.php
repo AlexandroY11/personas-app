8793406aa02b6f09f45885a05b378723de2b5297
Alexandro Yule Valencia <alexandroyule@gmail.com>
2024-03-26 00:45:13 -0500

    Footer y Nabvar

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer bg-dark">
+    <div class="container">
+        <span class="text-white">© 2024 Personas-App - Proyecto Laravel</span>
+        <span class="text-white">Author: Alexandro Yule Valencia - S5AN</span>
+        <span class="text-white"><a href="https://api.whatsapp.com/send?phone=573157298442" class="me-3" target="__blank"><i class="fab fa-whatsapp"></i>+57 3157298442</a></span>
+    </div>
+</footer>
