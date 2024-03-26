b1bf1752d153b1347d8f9a1fb3acc149b7e32b85
Alexandro Yule Valencia <alexandroyule@gmail.com>
2024-03-26 00:45:00 -0500

    Plantilla de Blade

---
+++ resources/views/layouts/plantilla.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    {{-- bootstrap --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
+    {{-- FontAwesome --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    {{-- Estilos personalizados --}}
+    <link href="{{ asset('css/components.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    {{-- TAILWIND --}}
+    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
+
+    <title>Personas-App || @yield('title')</title>
+</head>
+<body>
+    @include('layouts.partials.navbar')
+    <div class="container-fluid" id="div-principal">
+        <div class="container">
+            @yield('content')
+        </div>
+    </div>
+
+    @include('layouts.partials.footer')
+
+    {{-- Scripts al final del cuerpo del documento --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    {{-- JavaScript personalizado para el botón de WhatsApp --}}
+
+</body>
+</html>
